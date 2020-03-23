aab7b7b8a58fdaae64aa9fcc826fde1b9d64840b
Pengyuan Zhou <jackson.sun@163.com>
2020-03-23 20:57:25 +0800

    add springDemeDatabase

---
+++ springDemoDatababase/src/main/java/com/example/demo/DatabaseDempApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabaseDempApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DatabaseDempApplication.class, args);
+    }
+
+}
